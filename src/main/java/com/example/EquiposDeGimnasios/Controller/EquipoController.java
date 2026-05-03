c2d87c1530b76225ea2d52516fa5a83fc82b3c63
BenjaminDuran <benj.durans@duocuc.cl>
2026-05-03 12:12:13 -0400

    Update EquipoController.java
    Se actualiza para un completo CRUD en equipocontroller

--- src/main/java/com/example/EquiposDeGimnasios/Controller/EquipoController.java
+++ src/main/java/com/example/EquiposDeGimnasios/Controller/EquipoController.java
@@ -11,9 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/equipos")
+@RequestMapping("/api/v1/equipos")
 public class EquipoController {
-
     @Autowired
     private EquipoService service;
 
@@ -27,10 +26,15 @@ public class EquipoController {
         return new ResponseEntity<>(service.guardar(equipo), HttpStatus.CREATED);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Equipo> actualizar(@PathVariable Long id, @Valid @RequestBody Equipo e) {
+        e.setId(id);
+        return ResponseEntity.ok(service.guardar(e));
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         service.eliminarPorId(id);
         return ResponseEntity.noContent().build();
     }
-
 }
