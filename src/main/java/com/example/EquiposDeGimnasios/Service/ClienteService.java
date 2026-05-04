13d32ec73ccddbb0eb1b72a0ef6c0df857ec2a26
BenjaminDuran <benj.durans@duocuc.cl>
2026-05-03 23:32:32 -0400

    Update ClienteService.java
    Se actualiza clienteservice

--- src/main/java/com/example/EquiposDeGimnasios/Service/ClienteService.java
+++ src/main/java/com/example/EquiposDeGimnasios/Service/ClienteService.java
@@ -1,31 +1,36 @@
 package com.example.EquiposDeGimnasios.Service;
 
-
 import com.example.EquiposDeGimnasios.Model.Cliente;
 import com.example.EquiposDeGimnasios.Repository.ClienteRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Slf4j
 public class ClienteService {
-
     @Autowired
     private ClienteRepository clienteRepository;
 
     public List<Cliente> findAll() {
+        log.info("Logs: Listando todos los clientes desde la base de datos");
         return clienteRepository.findAll();
     }
 
+    public Optional<Cliente> findById(Long id) {
+        log.info("Logs: Buscando cliente con ID: {}", id);
+        return clienteRepository.findById(id);
+    }
+
     public Cliente save(Cliente cliente) {
+        log.info("Logs: Guardando cliente: {} {}", cliente.getNombres(), cliente.getApellidos());
         return clienteRepository.save(cliente);
     }
 
-    public Cliente findById(Long id) {
-        return clienteRepository.findById(id).orElse(null);
-    }
-
     public void delete(Long id) {
+        log.warn("Logs: Eliminando cliente con ID: {}", id);
         clienteRepository.deleteById(id);
     }
 }
